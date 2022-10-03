73be946c70bbd5fb5d89f21b50f454b667cf787f
mrkk7999 <82576116+mrkk7999@users.noreply.github.com>
2022-10-03 19:25:09 +0530

    added linked list basic structure

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -2,48 +2,45 @@ package main
 
 import "fmt"
 
-// Two structures
-// 1. Node -> individual node of linked list
+// Structure
+// Node -> individual node of linked list
 type node struct {
 	nodeVal  interface{}
 	nextNode *node
 }
 
-// 2. Linked list structure -> contains list of linked nodes
-type linkedList struct {
-	lenOfLL int
-	head    *node
-}
-
-func initEmptyList() *linkedList {
-	return &linkedList{
-		lenOfLL: 0,
-		head: &node{
-			nodeVal:  nil,
-			nextNode: nil,
-		},
+func initEmptyList() *node {
+	return &node{
+		nodeVal:  nil,
+		nextNode: nil,
 	}
 }
 
-func initWithHead(ele interface{}) *linkedList {
-	return &linkedList{
-		lenOfLL: 1,
-		head: &node{
-			nodeVal:  ele,
-			nextNode: nil,
-		},
+func initWithHead(ele interface{}) *node {
+	return &node{
+		nodeVal:  ele,
+		nextNode: nil,
 	}
 }
 
 func main() {
 	singleList := initEmptyList()
-	list := singleList.head
-	for i := 0; i < 10; i++ {
-		list.nodeVal = i
-		list = list.nextNode
-	}
-	list = singleList.head
-	for i := 0; i < 10; i++ {
-		fmt.Println(list.nodeVal)
-	}
+	singleList = initWithHead("Name :- ")
+	fmt.Println(singleList.nodeVal)
+	//tempNode := singleList
+	//// tempNode.nodeVal = "Mr."
+	//tempNode.nextNode = initWithHead("Mr.")
+	//tempNode = tempNode.nextNode
+	//tempNode.nextNode = initWithHead("Kiran")
+	//tempNode = tempNode.nextNode
+	//tempNode.nextNode = initWithHead("Arun")
+	//tempNode = tempNode.nextNode
+	//tempNode.nextNode = initWithHead("Kshirsagar")
+	//tempNode = tempNode.nextNode
+	//temp := singleList
+	//for temp.nextNode != nil {
+	//	fmt.Print(temp.nodeVal, "\t")
+	//	temp = temp.nextNode
+	//}
+	//fmt.Print(temp.nodeVal)
 }
