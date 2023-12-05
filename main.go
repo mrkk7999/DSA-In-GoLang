8130818531d29a7760d998710b120fc76cec6fe0
mrkk7999 <mrkk7999@gmail.com>
2023-12-06 01:09:58 +0530

    changes just to check if everything running properly

--- main.go
+++ main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("I will be writing DSA in golang")
+	fmt.Println("long time no see!")
+	fmt.Println("ohh you missed on consistency!")
+	fmt.Println("no worries")
+	fmt.Println("you can always pick again")
 }
